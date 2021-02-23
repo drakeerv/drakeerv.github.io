334538344a1d78bb9eef07f9ee7812bf5ec55fa4
drakeerv <60192521+drakeerv@users.noreply.github.com>
2021-02-22 20:06:38 -0500

    easter egg

--- Python_Minecraft_Clone_Website/jscode/main.js
+++ Python_Minecraft_Clone_Website/jscode/main.js
@@ -1,7 +1,14 @@
+var egg = new Egg();
+var egg_audio = new Audio("assets/egg.mp3")
+egg_audio.play();
+
 function init(e) {
     initdownload(e)
+    egg.addCode("up,up,down,down,left,right,left,right,b,a", function() {
+        egg_audio.play();
+    });
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
